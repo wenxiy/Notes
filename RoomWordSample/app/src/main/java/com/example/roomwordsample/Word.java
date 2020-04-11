87c49b326aa68fdc142b48083acbcc00f9c9b606
wenxiy <193367189@qq.com>
2020-04-11 19:42:28 +0800

    Room

---
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/Word.java
@@ -0,0 +1,23 @@
+package com.example.roomwordsample;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word{
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+    public Word(@Nullable String word)
+    {
+        this.mWord=word;
+    }
+    public String getWord()
+    {
+        return this.mWord;
+    }
+}
---
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/WordRepository.java
@@ -0,0 +1,26 @@
+package com.example.roomwordsample;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordRepository {
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+    WordRepository(Application application){
+        WordRoomDatabase db=WordRoomDatabase.getDatabase(application);
+        mWordDao=db.wordDao();
+        mAllWords=mWordDao.getAlphabetizedWords();
+    }
+    LiveData<List<Word>>getAllWords(){
+        return mAllWords;
+    }
+    void insert(Word word)
+    {
+        WordRoomDatabase.databaseWriteExecutor.execute(()->{
+            mWordDao.insert(word);
+        });
+    }
+}
