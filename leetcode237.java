b9e7cb94d82db734036c61e060a5ba4e021f406e
wenxiy <193367189@qq.com>
2020-07-04 19:38:37 +0800

    leetcode237

---
+++ leetcode237.java
@@ -0,0 +1,19 @@
+package com.company;
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
