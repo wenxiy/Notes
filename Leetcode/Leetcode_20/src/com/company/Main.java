f31e401eb5f1e37dfbbfb1638a1e6c42f91dd37d
wenxin <193367189@qq.com>
2020-11-08 08:49:18 +0800

    recently

---
+++ Leetcode/Leetcode_20/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Main {
+    /**
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+     * 
+     */
+    class Solution {
+        public boolean isValid(String s) {
+            int n = s.length();
+            if (n % 2 == 1) {
+                return false;
+            }
+
+            Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+                put(')', '(');
+                put(']', '[');
+                put('}', '{');
+            }};
+            Deque<Character> stack = new LinkedList<Character>();
+            for (int i = 0; i < n; i++) {
+                char ch = s.charAt(i);
+                if (pairs.containsKey(ch)) {
+                    if (stack.isEmpty() || stack.peek() != pairs.get(ch)) {
+                        return false;
+                    }
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+            return stack.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
